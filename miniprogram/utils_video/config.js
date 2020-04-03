203cf63d9112db2fb452799119af146203c14466
Fish08042 <37408049+Fish08042@users.noreply.github.com>
2020-04-03 16:47:17 +0800

    Update config.js

--- miniprogram/utils_video/config.js
+++ miniprogram/utils_video/config.js
@@ -2,8 +2,8 @@ var fileHost = "yjy08042.oss-cn-shanghai.aliyuncs.com"
 var config = {
   //aliyun OSS config
   uploadImageUrl: `${fileHost}`, //默认存在根目录，可根据需求改
-  AccessKeySecret: 'UVcF2iJbQXYeLtOb4WFCT4Hkq9TcwE',
-  OSSAccessKeyId: 'LTAI4FbfadbDpYz6PwB2B1Li',
+  AccessKeySecret: 'balabala',//这儿改成你自己的AccessKeySecret
+  OSSAccessKeyId: 'balabala',//这儿改成你自己的OSSAccessKeyId
   timeout: 87600 //这个是上传文件时Policy的失效时间
 };
 module.exports = config
